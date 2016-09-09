afac90a6d23d5f7187711e6a64d3dba387f07662
Nick Platt <platt.nicholas@gmail.com>
2016-09-09 13:57:10 -0400

    change header includes and include font as resource

--- Sources/DripIcons.h
+++ Sources/DripIcons.h
@@ -1,11 +1,4 @@
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for DripIcons.
 FOUNDATION_EXPORT double DripIconsVersionNumber;
-
-//! Project version string for DripIcons.
 FOUNDATION_EXPORT const unsigned char DripIconsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <DripIcons/PublicHeader.h>
-
-
